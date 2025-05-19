4c0a308be65067f19a038ed3452d5cfa11c764db
LyenZo <marcosj.9807@gmail.com>
2025-05-19 10:26:04 -0600

    Commit de creación de API

---
+++ db.js
@@ -0,0 +1,18 @@
+const mysql = require('mysql');
+
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  database: 'hortacom_shs'
+});
+
+connection.connect((err) => {
+  if (err) {
+    console.error('Error de conexión a la base de datos:', err);
+    return;
+  }
+  console.log('Conexión a la base de datos exitosa');
+});
+
+module.exports = connection;
